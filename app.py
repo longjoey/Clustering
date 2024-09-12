f23f1f2fbde3ebac48c17dc4bb2b9b9dee33c311
longjoey <104349363+longjoey@users.noreply.github.com>
2024-09-13 02:28:00 +0800

    app.py

--- app.py
+++ app.py
@@ -2,11 +2,28 @@
 import streamlit as st
 
 # Title of the app
-st.title('My Streamlit App')
+st.title('FB Live Seller Clustering')
 
 # Some text and an input box
-name = st.text_input("What's your name?")
+model = st.text_input("Select a clustering model")
 
 # If the user inputs their name, show a greeting
-if name:
-    st.write(f"Hello, {name}!")
+model = st.selectbox(
+    'Select a clustering model:',
+    ('GMM', 'Hierarchical Clustering', 'DBSCAN', 'Self-Organizing Maps')
+)
+
+if model == 'GMM' :
+    n_components = st.slider(
+        'Number of components:', 1, 10, 3
+    )
+
+show_diag = st.checkbox('Show Diagram')
+
+
+
+
+
+
+
+
