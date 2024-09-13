b530b26128c0fdb6c5e6ec58313e85b950dae42a
longjoey <104349363+longjoey@users.noreply.github.com>
2024-09-13 10:34:33 +0800

    app.py

--- app.py
+++ app.py
@@ -8,6 +8,13 @@ import pandas as pd
 # Title of the app
 st.title('FB Live Seller Clustering')
 
+url = https://raw.githubusercontent.com/longjoey/Clustering/main/X_pca.csv
+
+def load_data():
+    return pd.read_csv(url)
+
+X_pca = load_data()
+
 # If the user inputs their name, show a greeting
 model = st.selectbox(
     'Select a clustering model:',
@@ -19,7 +26,20 @@ if model == 'GMM' :
         'Number of components:', 1, 10, 3
     )
 
+    gmm = GaussianMixture(covariance_type = 'diag', n_components = n_components, random_state = 42) 
+    gmm.fit(X_pca)  
+
+    cluster_labels = gmm.predict(X_pca)
+
+    data_with_clusters = X_pca.copy()
+    data_with_clusters['Cluster'] = cluster_labels
     
+    # Show the resulting clusters
+    st.write("Clustering result:")
+    st.write(data_with_clusters)
+
+    # Optionally, display a message about the clustering
+    st.write(f'GMM clustered the data into {n_components} clusters.')
 
 show_diag = st.checkbox('Show Diagram')
 
